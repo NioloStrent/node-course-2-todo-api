cf7860b14810bd73ece5f475c7782a20169cb9a8
Niolo Strent <keithleidy1@gmail.com>
2017-04-17 15:10:30 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server')
+
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('58f122fc237aa200c65f58f2')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.collection('users').findOneAndUpdate({
+    _id: new ObjectID('58ee7a1386f4ac0584960547')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
